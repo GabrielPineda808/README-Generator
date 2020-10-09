73d0848e011281ffb4b9991b7296537f1fa79fa5
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-09 16:05:57 -0700

    added the template for the readme file and theen a function that runs it all after recieving the answerss from the user using inquirer

--- index.js
+++ index.js
@@ -36,7 +36,51 @@ function prompt(){
             type: "input",
             message: "What is your github username? ",
             name: "github"
+        },
+        {
+            type: "input",
+            message: "Are there any tests? ",
+            name: "test"
+        },
+        {
+            type: "input",
+            message: "Any questions? ",
+            name: "question"
         }
-    ])
+    ]).then()
 }
 
+function generatemd(data) {
+    return ` 
+# ${data.projectTitle}
+  ${data.summaryProject}
+  
+  ___Table-of-Contents___
+  1. [How to install](#install)
+  2. [How to Use](#usage)
+  3. [Contributions](#contributors)
+  4. [Tests](#tests)
+  5. [Questions](#questions)
+  
+  <a id="install">__Installation:__</a>
+  ${data.install}
+  
+  <a id="usage">__Usage:__</a>
+  ${data.usage}
+  
+  <a id="contributors">__Contributors:__</a>
+  ${data.contributors}
+  <a id="tests">__Tests:__</a>
+  ${data.test}
+  <a id="questions">__Questions:__</a>
+  ${data.question}
+  ![License: None](https://img.shields.io/badge/License-None-brightgreen)`;
+  
+}
+
+prompt().then(function(data){
+    const readme = generatemd(data);
+    return writeFileAsync("README.md", readme);
+}).then(function(err){
+    console.log(err);
+})
